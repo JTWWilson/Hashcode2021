f651791be0cd04814387074c5a95327e7f464087
Jacqui H <jacqui.henes@gmail.com>
2021-02-25 18:57:56 +0000

    input start

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 from format_data import load_file
 
-
 def main():
-    load_file('main.py')
-
-
+	# Input processing
+    input_lines = load_file('./datasets/a.txt').split("\n")[:-1]
+    time, intersections, streets, cars, points = input_lines[0].split(" ")
+    # List of intersections
+    street_info = input_lines[1:-int(cars)]
+    #print(street_info)
+    intersection_dict = {}
+    for street in street_info:
+    	vals = street.split(" ")
+    	start = vals[0]
+    	end = vals[1]
+    	st_name = vals[2]
+    	if start not in intersection_dict:
+    		intersection_dict[start] = ([st_name] , [])
+    	else:
+    		intersection_dict[start][0].append(st_name)
+    	if end not in intersection_dict:
+    		intersection_dict[end] = ([], [st_name])
+    	else:
+    		intersection_dict[end][1].append(st_name)
+    	
+    #print(intersection_dict)
+    
 if __name__ == '__main__':
     main()
+
