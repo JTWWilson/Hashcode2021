1d9fbb1e4416ce11d3714593196cf13a9469fc50
maxwarren@live.co.uk <max@max-pc.lan>
2021-02-25 18:43:23 +0000

    added puesdocode for initial solution

---
+++ optimizer.py
@@ -0,0 +1,12 @@
+
+
+def find_solution(inputs):
+    # for car in list of cars
+    #   for road in car's route
+    # add volume information to road
+
+    # for intersection in intersections
+    #   for input_road in intersection
+    #       compare intersections
+    #sort volumes
+    #pick based on volume
